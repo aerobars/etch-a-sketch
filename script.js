670252b4a8f52e33f512bdc0ec93b1c0f8c6c041
Adrian <adrianbyers@gmail.com>
2023-04-18 23:20:19 -0400

    Create script.js, add global variables and psuedocode

---
+++ script.js
@@ -0,0 +1,14 @@
+//set global variables
+const container = document.querySelector('#container');
+let xAxis = 16;
+let yAxis = 16;
+
+
+//create a 16x16 grid of square divs using documentFragment approach
+//create documentFragment
+//nested for loops
+//first loop make yAxis number of divs
+//set display of each yAxis div to flex
+//second loop make xAxis number of divs within each yAxis div
+//set xAxis div dimensions (5x5)
+//append documentFragment to container
