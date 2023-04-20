d8da1e22d6d0332e547ea3f6606aa0afdca0ac2b
Adrian <adrianbyers@gmail.com>
2023-04-19 20:45:19 -0400

    Add hover effect code, remove pseudocode

--- script.js
+++ script.js
@@ -3,23 +3,13 @@ const container = document.querySelector('#container');
 let x = 16;
 let y = 16;
 
-
-//create a 16x16 grid of square divs using documentFragment approach
-//create documentFragment
-//nested for loops
-//first loop make yAxis number of divs
-//set display of each yAxis div to flex
-//second loop make xAxis number of divs within each yAxis div
-//set xAxis div dimensions (5x5)
-//append documentFragment to container
-
-//function generateGride()
+//function generateGrid() {
   let fragment = new DocumentFragment()
 
   for (let i = 0; i < y; i++) {
     let yAxis = document.createElement('div');
-    yAxis.classList.add('grid');
-    yAxis.setAttribute('style', 'height: 10px; width: auto; display: flex; flex-direction: row;');
+    //yAxis.classList.add('grid');
+    yAxis.setAttribute('style', 'height: 10px; width: 200px; display: flex; flex-direction: row;');
     for (let j = 0; j < x; j++) {
       let xAxis = document.createElement('div');
       xAxis.classList.add('grid');
@@ -28,4 +18,13 @@ let y = 16;
     }
     fragment.appendChild(yAxis);
   }
+
   container.appendChild(fragment);
+//}
+
+const hover = document.querySelectorAll('.grid');
+hover.forEach((cell) => {
+  cell.addEventListener('mouseover', () => {
+    cell.classList.toggle ('moused');
+  })
+});
