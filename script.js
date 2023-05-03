732917a4f590777600e1e43b721d95748beb1c26
Adrian <adrianbyers@gmail.com>
2023-05-02 22:18:50 -0400

    Add button even listener and grid clearing function

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 //set global variables
 const container = document.querySelector('#container');
-let x = 100;
-let y = 100;
+let size = 100;
 
 //function generateGrid() {
   let fragment = new DocumentFragment()
 
-  for (let i = 0; i < y; i++) {
+  for (let i = 0; i < size; i++) {
     let yAxis = document.createElement('div');
-    //yAxis.classList.add('grid');
+    yAxis.classList.add('grid');
     yAxis.setAttribute('style', 'width: 500px; display: flex; flex-direction: row;');
-    yAxis.style.height = 500 / y + 'px';
-    for (let j = 0; j < x; j++) {
+    yAxis.style.height = 500 / size + 'px';
+    for (let j = 0; j < size; j++) {
       let xAxis = document.createElement('div');
-      xAxis.classList.add('grid');
-      xAxis.style.height = 500 / y + 'px';
-      xAxis.style.width = 500 / x + 'px';
+      xAxis.classList.add('x-axis');
+      xAxis.style.height = 500 / size + 'px';
+      xAxis.style.width = 500 / size + 'px';
       yAxis.appendChild(xAxis);
     }
     fragment.appendChild(yAxis);
@@ -24,12 +23,27 @@ let y = 100;
   container.appendChild(fragment);
 //}
 
-const hover = document.querySelectorAll('.grid');
+const hover = document.querySelectorAll('.x-axis');
 hover.forEach((cell) => {
   cell.addEventListener('mouseover', () => {
     cell.classList.toggle ('moused');
   })
 });
 
-const resize = document.querySelector('#resize');
-resize.addEventListener()
+const resize = document.getElementById('resize');
+resize.addEventListener('click', () => {
+  //size = window.prompt("Please enter grid dimension");
+  gridClear
+  console.log('test');
+})
+
+function gridClear() {
+  const clearCell = document.querySelectorAll('.x-axis');
+  const clearGrid = document.querySelectorAll('.grid');
+  clearCell.forEach((cell) => {
+    clearGrid.removeChild(cell);
+  })
+  clearGrid.forEach((cell) => {
+    container.removeChild(cell);
+  })
+}
